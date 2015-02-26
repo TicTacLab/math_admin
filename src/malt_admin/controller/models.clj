bd9b59ddc2e7f65ef60797ecc27b024abc5e3080
serzh <nechyporchuk@gmail.com>
2015-02-26 15:40:43 +0200

    add upload and index pages

--- src/malt_admin/controller/models.clj
+++ src/malt_admin/controller/models.clj
@@ -4,21 +4,33 @@
             [malt-admin.form.model :as form]
             [formative.parse :as fp]
             [ring.util.response :as res]
-            [formative.core :as f]))
+            [formative.core :as f]
+            [malt-admin.storage.models :as storage]))
 
-(defn index [req]
-  (render "models/index" req {}))
+(defn index [{{storage :storage} :web :as req}]
+  (render "models/index" req {:models (storage/get-models storage)}))
 
 (defn upload [{:keys [problems params] :as req}]
   (render "models/upload" req {:upload-form (assoc form/upload-form
                                               :action "/models"
                                               :method "POST"
-                                              :values (merge {:in-sheet-name  "IN"
-                                                              :out-sheet-name "OUT"}
+                                              :values (merge {:in_sheet_name  "IN"
+                                                              :out_sheet_name "OUT"}
                                                              params)
                                               :problems problems)}))
 
-(defn do-upload [{params :params :as req}]
+(defn ^:private file->bytes [file]
+  (.getBytes (slurp (:tempfile file))))
+
+(defn ^:private prepare-file-attrs [{{:keys [tempfile filename]} :file :as params}]
+  (assoc params
+    :file (.getBytes (slurp tempfile))
+    :file_name filename))
+
+(defn do-upload [{params :params
+                  {storage :storage} :web
+                  :as req}]
   (fp/with-fallback #(malt-admin.controller.models/upload (assoc req :problems %))
-    (let [values (fp/parse-params form/upload-form params)]
-      {:body (pr-str (slurp (:tempfile (:file values))))})))
+    (let [values (prepare-file-attrs (fp/parse-params form/upload-form params))]
+      (storage/write-model! storage values)
+      (res/redirect "/models"))))
