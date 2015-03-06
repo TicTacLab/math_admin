a598058293806e6290b6b2378ce564a0f7a96db7
serzh <nechyporchuk@gmail.com>
2015-03-06 10:13:52 +0200

    push scripts

--- scripts/init-cassandra.sh
+++ scripts/init-cassandra.sh
@@ -1,4 +1,4 @@
 docker run -it --rm -v `pwd`:/data spotify/cassandra:base cqlsh -f init-keyspace.cql $1
-docker run -it --rm -v `pwd`:/data spotify/cassandra:base cqlsh -f init-schema.cql $1
-docker run -it --rm -v `pwd`:/data spotify/cassandra:base cqlsh -f init-config.cql $1
-docker run -it --rm -v `pwd`:/data spotify/cassandra:base cqlsh -f init-settings.cql $1
+docker run -it --rm -v `pwd`:/data spotify/cassandra:base cqlsh -f init-schema.cql -k malt $1
+docker run -it --rm -v `pwd`:/data spotify/cassandra:base cqlsh -f init-config.cql -k malt $1
+docker run -it --rm -v `pwd`:/data spotify/cassandra:base cqlsh -f init-settings.cql -k malt $1
