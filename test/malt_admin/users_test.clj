9b08918cf2e39bfbd8468f7eac98a35325540c6f
Eugene Linchenko <e.linchenko@favbet.com>
2015-03-12 09:59:59 +0200

    fix user tests

--- test/malt_admin/users_test.clj
+++ test/malt_admin/users_test.clj
@@ -7,34 +7,46 @@
 (deftest models-test
   (t/with-system [s (test-system environ/env)]
     (let [b (t/start-browser! s)]
-      (signin b)
-      (go b "/users")
-      (is (= 1 (count (elements b :.user))) "Should be only one user")
+
+      (testing "Default users"
+        (signin b)
+        (go b "/users")
+        (is (= 2 (count (elements b :.user))) "Should be only two users")
+        (signout b))
+
+      (w/implicit-wait b 1)
 
       (testing "Create"
+        (signin b)
+        (go b "/users")
         (click b "Create")
         (fill-in b "Name" "Superman")
         (fill-in b "Login" "super-duper")
+        (click b :#field-is_admin)
         (fill-in b "Password" "super-password")
         (fill-in b "Password confirmation" "super-password")
         (click b "Submit")
 
-        (is (= 2 (count (elements b :.user))) "Should be only two users"))
+        (is (= 3 (count (elements b :.user))) "Should be only three users")
+        (signout b))
 
       (testing "Signin newly created user"
-        (signout b)
         (signin b "super-duper" "super-password")
         (is (= "You successfully signed in" (text b :#flash)) "User should be allowed to singin")
-        (signout b)
-        (signin b))
+        (signout b))
 
       (testing "Edit"
+        (signin b)
+        (go b "/users")
         (click (second (elements b "Edit")))
         (fill-in b "Name" "God")
         (click "Submit")
-        (is (= "God" (text b (second (elements b :.user-name)))) "Should change user name"))
+        (is (= "God" (text b (second (elements b :.user-name)))) "Should change user name")
+        (signout b))
 
       (testing "Password change"
+        (signin b)
+        (go b "/users")
         (click (second (elements b "Password")))
         (fill-in b "Password" "simple-password")
         (fill-in b "Password confirmation" "simple-password")
@@ -42,18 +54,24 @@
         (signout b)
         (signin b "super-duper" "simple-password")
         (is (= "You successfully signed in" (text b :#flash)) "User should be allowed to singin")
-        (signout b)
-        (signin b))
-      
+        (signout b))
+
       (testing "Activation/Deactivation"
+        (signin b)
+        (go b "/users")
         (click (second (elements b "Deactivate")))
-        (is (= "inactive" (text b (second (elements b :.user-status)))) "User status should be changed to inactive")
+        (is (= "inactive" (text b (nth (elements b :.user-status) 2))) "User status should be changed to inactive")
+        (signout b)
+
         (signin b "super-duper" "simple-password")
         (is (re-find #"Invalid login or password"
                      (text b :.form-problems))
             "Inactive user should not be able to singin")
+
         (signin b)
+        (go b "/users")
         (click b "Activate")
         (signout b)
+
         (signin b "super-duper" "simple-password")
         (is (= "You successfully signed in" (text b :#flash)) "Active user should be allowed to singin")))))
