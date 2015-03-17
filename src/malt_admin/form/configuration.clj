73a59a9d22ccff34c50975c8cf8e363ad8854b8a
Eugene Linchenko <e.linchenko@favbet.com>
2015-03-17 15:09:44 +0200

    add confing and settings form hints

--- src/malt_admin/form/configuration.clj
+++ src/malt_admin/form/configuration.clj
@@ -1,10 +1,10 @@
 (ns malt-admin.form.configuration)
 
 (def config
-  {:fields [{:name :session-ttl :type :text :datatype :int :label "Session TTL (min)"}
-            {:name :cache-on :type :checkbox :datatype :boolean :value true}
-            {:name :rest-port :type :text :datatype :int}
-            {:name :hosts :type :text :label "Cassandra hosts"}]
+  {:fields [{:name :session-ttl :type :text :datatype :int :label "Session TTL (1..60 minutes)"}
+            {:name :cache-on :type :checkbox :datatype :boolean :value true :label "Turn on results caching?"}
+            {:name :rest-port :type :text :datatype :int :label "REST port (on which malt will listen requests)"}
+            {:name :hosts :type :text :label "Cassandra cluster hosts"}]
    :validations [[:min-val 1 :session-ttl]
                  [:max-val 60 :session-ttl]
                  [:required [:session-ttl
--- src/malt_admin/form/settings.clj
+++ src/malt_admin/form/settings.clj
@@ -1,6 +1,6 @@
 (ns malt-admin.form.settings)
 
 (def settings
-  {:fields [{:name :profiling-malt-host :type :text}
+  {:fields [{:name :profiling-malt-host :type :text :label "Profiling malt host (malt on which model profiling will run)"}
             {:name :profiling-malt-port :type :number}]
    :validations [[:required [:profiling-malt-host :profiling-malt-port]]]})
