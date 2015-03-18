6f15d4f64647bf2796f4fd3b5c53163c4c39e589
Eugene Linchenko <e.linchenko@favbet.com>
2015-03-18 20:15:12 +0200

    fix cqlsh script

--- scripts/run-cqlsh.sh
+++ scripts/run-cqlsh.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it --rm spotify/cassandra:base cqlsh $@
+docker run -it --rm spotify/cassandra:base cqlsh $(docker inspect --format '{{ .NetworkSettings.IPAddress }}' cassandra)
