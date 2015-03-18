f5044e2679a8f48fd0c5e9b1937270354013413d
Eugene Linchenko <e.linchenko@favbet.com>
2015-03-18 09:46:00 +0200

    bind c* to localhost only

--- scripts/init-cassandra.sh
+++ scripts/init-cassandra.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker run -it --rm -v `pwd`:/data spotify/cassandra:base cqlsh -f init-keyspace.cql $1
-docker run -it --rm -v `pwd`:/data spotify/cassandra:base cqlsh -f init-schema.cql -k malt $1
-docker run -it --rm -v `pwd`:/data spotify/cassandra:base cqlsh -f init-config.cql -k malt $1
-docker run -it --rm -v `pwd`:/data spotify/cassandra:base cqlsh -f init-settings.cql -k malt $1
+docker run -it --rm -v `pwd`:/data spotify/cassandra:base cqlsh -f init-keyspace.cql $(docker inspect --format '{{ .NetworkSettings.IPAddress }}' cassandra)
+docker run -it --rm -v `pwd`:/data spotify/cassandra:base cqlsh -f init-schema.cql -k malt $(docker inspect --format '{{ .NetworkSettings.IPAddress }}' cassandra)
+docker run -it --rm -v `pwd`:/data spotify/cassandra:base cqlsh -f init-config.cql -k malt $(docker inspect --format '{{ .NetworkSettings.IPAddress }}' cassandra)
+docker run -it --rm -v `pwd`:/data spotify/cassandra:base cqlsh -f init-settings.cql -k malt $(docker inspect --format '{{ .NetworkSettings.IPAddress }}' cassandra)
--- scripts/run-cassandra.sh
+++ scripts/run-cassandra.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d -p 9160:9160 -p 9042:9042 --name cassandra spotify/cassandra
+docker run -d -p 127.0.0.1::9160 -p 127.0.0.1::9042 --name cassandra spotify/cassandra
