362729c060167c700481d7410a2c1d021510be99
Eugene Linchenko <e.linchenko@favbet.com>
2015-03-18 11:13:18 +0200

    fix double signin

--- src/malt_admin/controller/auth.clj
+++ src/malt_admin/controller/auth.clj
@@ -3,25 +3,31 @@
             [malt-admin.form.auth :as form]
             [malt-admin.helpers :refer [redirect-with-flash error!]]
             [malt-admin.storage.auth :as storage]
-            [formative.parse :as fp]))
+            [formative.parse :as fp]
+            [ring.util.response :as res]))
 
-(defn index [{:keys [problems params]
+(defn index [{:keys [problems params session-id]
               :as   req}]
-  (render "auth/index" req {:form (assoc form/signin
-                                    :problems problems
-                                    :values (select-keys params [:login])
-                                    :action "/auth"
-                                    :method "POST")}))
+  (if session-id
+      (res/redirect "/")                                    ;; if already signed in, do nothing
+      (render "auth/index" req {:form (assoc form/signin
+                                        :problems problems
+                                        :values (select-keys params [:login])
+                                        :action "/auth"
+                                        :method "POST")})))
 
 (defn sign-in [{params             :params
                {storage :storage} :web
+                session-id :session-id
                :as                req}]
-  (fp/with-fallback #(malt-admin.controller.auth/index (assoc req :problems %))
-   (let [{:keys [login password]} (fp/parse-params form/signin params)]
-     (if-let [session-data (storage/sign-in storage login password)]
-       (-> (redirect-with-flash "/models" {:success "You successfully signed in"})
-           (assoc :session session-data))
-       (error! [:login :password] "Invalid login or password")))))
+  (if session-id
+    (res/redirect "/")                                      ;; if already signed in, do nothing
+    (fp/with-fallback #(malt-admin.controller.auth/index (assoc req :problems %))
+      (let [{:keys [login password]} (fp/parse-params form/signin params)]
+        (if-let [session-data (storage/sign-in storage login password)]
+          (-> (redirect-with-flash "/models" {:success "You successfully signed in"})
+              (assoc :session session-data))
+          (error! [:login :password] "Invalid login or password"))))))
 
 (defn sign-out [{{storage :storage} :web
                  session-id :session-id}]
