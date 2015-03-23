6a121a47b6cd2590ad5b51c8094dea86ecc054a0
serzh <nechyporchuk@gmail.com>
2015-03-23 10:59:17 +0200

    fix audit who

--- src/malt_admin/audit.clj
+++ src/malt_admin/audit.clj
@@ -4,7 +4,7 @@
   (:import (java.util Date)))
 
 (defn audit [req action subject]
-  (log/info (json/generate-string {:who     (get-in req [:session :user])
+  (log/info (json/generate-string {:who     (get-in req [:session :login])
                                    :when    (.getTime (Date.))
                                    :action  action
                                    :subject subject})))
