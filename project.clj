f673e660a5fd10bed223fbc8c285eee0a4aacb3b
ci <you@example.com>
2015-04-23 12:01:12 +0300

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject malt_admin "0.1.0"
+(defproject malt_admin "0.1.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
