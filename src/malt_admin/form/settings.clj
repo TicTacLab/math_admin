f4f8201f536ee0b0ee3cad795dcf31d78edb2799
D. Omelechko <dvarkin@gmail.com>
2015-05-20 15:20:08 +0300

    add filler-add to admin iface

--- src/malt_admin/form/settings.clj
+++ src/malt_admin/form/settings.clj
@@ -2,5 +2,7 @@
 
 (def settings
   {:fields [{:name :profiling-malt-host :type :text :label "Profiling malt host" :note "malt on which model profiling will run"}
-            {:name :profiling-malt-port :type :number}]
-   :validations [[:required [:profiling-malt-host :profiling-malt-port]]]})
+            {:name :profiling-malt-port :type :number}
+            {:name :cache-filler-addr :type :text :label "Cache filler address" :note "malt engine addr (X.X.X.X:XXXX) for execute filler requests"}]
+   :validations [[:required [:profiling-malt-host :profiling-malt-port :cache-filler-addr]]]}
+  )
