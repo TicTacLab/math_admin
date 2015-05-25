71fa5e1b830506e8fc18da7c81c0e32b1a5de736
serzh <nechyporchuk@gmail.com>
2015-05-25 10:42:46 +0300

    use config instead of environ

--- src/migration/v005_add_cache_q_table.clj
+++ src/migration/v005_add_cache_q_table.clj
@@ -2,11 +2,12 @@
   (:require [migration.util :refer [with-component]]
             [malt-admin.system :as sys]
             [clojurewerkz.cassaforte.cql :as cql]
-            [clojurewerkz.cassaforte.query :as q])
+            [clojurewerkz.cassaforte.query :as q]
+            [malt-admin.config :as c])
   (:gen-class))
 
 (defn -main [& args]
-  (with-component [storage (-> environ/env
+  (with-component [storage (-> @c/config
                                sys/new-system
                                :storage)]
                   (let [c (:conn storage)]
