bd5d4f5b29f4610b8a29f0e70c214ed216842038
Eugene Linchenko <e.linchenko@favbet.com>
2015-09-14 13:57:39 +0300

    rename malt -> math engine, remove cache filler settings entry

--- test/malt_admin/settings_test.clj
+++ test/malt_admin/settings_test.clj
@@ -13,28 +13,25 @@
       (is (= 3 (count (elements b [:.form-shell :.fieldset-profiling-malt-host :input]))) "Should have 3 input fields")
 
       (testing "Successul update"
-        (fill-in b "Profiling malt host" "127.0.0.1")
-        (fill-in b "Profiling malt port" "6666")
-        (fill-in b "Cache filler address" "127.0.0.1:6666")
+        (fill-in b "Profiling math engine host" "127.0.0.1")
+        (fill-in b "Profiling math engine port" "6666")
 
         (click b "Update")
         (t/wait-condition b (ExpectedConditions/alertIsPresent))
         (accept b)
 
         (is (= "127.0.0.1" (value b "Profiling malt host")))
-        (is (= "6666" (value b "Profiling malt port")))
-        (is (= "127.0.0.1:6666" (value b "Cache filler address"))))
+        (is (= "6666" (value b "Profiling malt port"))))
 
 
       (testing "Required filds validation"
-        (fill-in b "Profiling malt host" "")
-        (fill-in b "Profiling malt port" "")
-        (fill-in b "Cache filler address" "")
+        (fill-in b "Profiling math engine host" "")
+        (fill-in b "Profiling math engine port" "")
 
         (click b "Update")
         (t/wait-condition b (ExpectedConditions/alertIsPresent))
         (accept b)
 
         (wait-until b (fn [b] (exists? b [:.form-problems :.control-label])) 5000 0)
-        (is (= "Profiling malt host, Profiling malt port, Cache filler address: must not be blank"
+        (is (= "Profiling math engine host, Profiling math engine port: must not be blank"
                (text b  :.control-label)))))))
