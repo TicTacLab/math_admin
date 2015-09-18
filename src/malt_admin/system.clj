2a61ca64929500780ca09526cf5c1825df56f5c3
Eugene Linchenko <e.linchenko@favbet.com>
2015-09-18 16:51:00 +0300

    start zabbix only if host specified

--- src/malt_admin/system.clj
+++ src/malt_admin/system.clj
@@ -6,21 +6,18 @@
             [malt-admin.storage :as storage]))
 
 (defn new-system [config]
-  (component/system-map
-   :web (component/using
-         (web/new-web config)
-         [:storage :offloader])
-
-   ;; TODO: make filler better
-   #_:filler #_(component/using
-             (filler/new-filler {})
-             [:storage])
-   :offloader (component/using
-                (off/new-offloader config)
-                [:storage])
-   :storage (storage/new-storage config)
-   :zabbix-reporter (zabbix/new-zabbix-reporter
-                      {:hostname         (:monitoring-hostname config)
-                       :zabbix-host      (:zabbix-host config)
-                       :zabbix-port      (:zabbix-port config)
-                       :interval-minutes 10})))
+  (component/map->SystemMap
+    (merge
+      {:web       (component/using
+                    (web/new-web config)
+                    [:storage :offloader])
+       :offloader (component/using
+                    (off/new-offloader config)
+                    [:storage])
+       :storage   (storage/new-storage config)}
+      (when (:zabbix-host config)
+        {:zabbix-reporter (zabbix/new-zabbix-reporter
+                            {:hostname         (:monitoring-hostname config)
+                             :zabbix-host      (:zabbix-host config)
+                             :zabbix-port      (:zabbix-port config)
+                             :interval-minutes 10})}))))
