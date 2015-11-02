48ca9aef6a1c01f7f3b3b8fd44bbc1d38709f295
Dmitriy Ferens <ferensdima@gmail.com>
2015-11-02 10:00:37 +0200

    Added error handling to index page of sengine

--- src/malt_admin/controller/sengine_files.clj
+++ src/malt_admin/controller/sengine_files.clj
@@ -1,12 +1,41 @@
 (ns malt-admin.controller.sengine-files
   (:require [org.httpkit.client :as http]
             [cheshire.core :as json]
-            [malt-admin.view :refer (render u)]))
+            [malt-admin.view :refer (render u)]
+            [clojure.tools.logging :as log]
+            [clojure.string :as s]))
+
+(defn wrap-error [log-error-prefix error]
+  (if (instance? Throwable error)
+    (do
+      (log/error error log-error-prefix)
+      [:error (.getLocalizedMessage error)])
+    (do
+      (log/error (str log-error-prefix error))
+      [:error error])))
+
+(defn error-response->string-message [response]
+  (->> response
+       :errors
+       (map :message)
+       (s/join " ")))
 
 (defn index
   [{{:keys [sengine-addr]} :web :as req}]
-  (let [url (format "http://%s/files" sengine-addr)
-        {:keys [status body]} @(http/get url)
-        files (:data (json/parse-string body true))]
-    (>trace files)
-    (render "sengine/index" req {:files files})))
+  (let [url (format "http://%s/file" sengine-addr)
+        {:keys [status body error]} @(http/get url)
+        response (json/parse-string body true)
+        error-prefix "Error while getting files: "]
+    (cond
+      error (render "sengine/index" (->> error
+                                         (wrap-error error-prefix)
+                                         (vector)
+                                         (into {})
+                                         (assoc req :flash)) {})
+      (not= status 200) (render "sengine/index" (->> response
+                                                     (error-response->string-message)
+                                                     (wrap-error error-prefix)
+                                                     (vector)
+                                                     (into {})
+                                                     (assoc req :flash)) {})
+      :else (render "sengine/index" req {:files (:data response)}))))
