18351953ec7e816895399d5bd73c66fff461dc3a
Dmitriy Ferens <ferensdima@gmail.com>
2015-11-02 17:53:38 +0200

    Added basic profile page

--- src/malt_admin/controller/sengine_files.clj
+++ src/malt_admin/controller/sengine_files.clj
@@ -4,21 +4,20 @@
             [clojure.tools.logging :as log]
             [clojure.string :as s]
             [formative.parse :as fp]
-            [malt-admin.view :refer (render u)]
+            [malt-admin.view :refer (render u render-with-success render-with-error)]
             [malt-admin.helpers :refer [redirect-with-flash error!]]
             [malt-admin.form.model :as form]
-            [malt-admin.audit :as audit]
-            [clojure.java.io :as io])
+            [malt-admin.audit :as audit])
   (:import (java.util UUID)))
 
 (defn wrap-error [log-error-prefix error]
   (if (instance? Throwable error)
     (do
       (log/error error log-error-prefix)
-      [:error (.getLocalizedMessage error)])
+      [nil (.getLocalizedMessage error)])
     (do
       (log/error (str log-error-prefix error))
-      [:error error])))
+      [nil error])))
 
 (defn error-response->string-message [response]
   (->> response
@@ -26,25 +25,25 @@
        (map :message)
        (s/join " ")))
 
+(defn check-response [resp error-prefix]
+  (let [{:keys [status body error]} resp
+        response (json/parse-string body true)]
+    (cond
+      error (wrap-error error-prefix error)
+      (not= status 200) (->> response
+                             (error-response->string-message)
+                             (wrap-error error-prefix))
+      :else [(:data response) nil])))
+
 (defn index
   [{{:keys [sengine-addr]} :web :as req}]
   (let [url (format "http://%s/files" sengine-addr)
-        {:keys [status body error]} @(http/get url)
-        response (json/parse-string body true)
-        error-prefix "Error while getting files: "]
-    (cond
-      error (render "sengine/index" (->> error
-                                         (wrap-error error-prefix)
-                                         (vector)
-                                         (into {})
-                                         (assoc req :flash)) {})
-      (not= status 200) (render "sengine/index" (->> response
-                                                     (error-response->string-message)
-                                                     (wrap-error error-prefix)
-                                                     (vector)
-                                                     (into {})
-                                                     (assoc req :flash)) {})
-      :else (render "sengine/index" req {:files (:data response)}))))
+        resp @(http/get url)
+        error-prefix "Error while getting files: "
+        [files error] (check-response resp error-prefix)]
+    (if error
+      (render-with-error "sengine/index" req error {})
+      (render "sengine/index" req {:files files}))))
 
 (defn upload [{:keys [problems params] :as req}]
   (render "sengine/upload" req {:upload-form (assoc form/upload-form
@@ -64,35 +63,24 @@
     {:keys [sengine-addr]} :web
     :as req}]
   (fp/with-fallback #(upload (assoc req :problems %))
-    (let [values (check-empty-file (fp/parse-params form/upload-form params))
-          error-prefix "Error while uploading file to sengine: "]
+    (let [values (check-empty-file (fp/parse-params form/upload-form params))]
       (cond
         (not (contains? values :file))
         (error! [:file] "You should specify file!")
-
         :else
         (let [{:keys [id file]} values
               {:strs [tempfile filename]} file
+              error-prefix "Error while uploading file to sengine: "
               url (format "http://%s/files/%s/upload" sengine-addr id)
-              {:keys [status body error]} @(http/post url {:multipart [{:name     "file"
-                                                                        :content  tempfile
-                                                                        :filename filename}]})
-              response (json/parse-string body true)]
-          (cond
-            error (render "sengine/upload" (->> error
-                                                (wrap-error error-prefix)
-                                                (vector)
-                                                (into {})
-                                                (assoc req :flash)) {})
-            (not= status 200) (render "sengine/upload" (->> response
-                                                            (error-response->string-message)
-                                                            (wrap-error error-prefix)
-                                                            (vector)
-                                                            (into {})
-                                                            (assoc req :flash)) {})
-            :else (do
-                    (audit/info req :upload-sengine-file (dissoc values :file))
-                    (redirect-with-flash "/sengine/files" {:success "DONE"}))))))))
+              resp @(http/post url {:multipart [{:name     "file"
+                                                 :content  tempfile
+                                                 :filename filename}]})
+              [_ error] (check-response resp error-prefix)]
+          (if error
+            (render-with-error "sengine/upload" req error {})
+            (do
+              (audit/info req :upload-sengine-file (dissoc values :file))
+              (redirect-with-flash "/sengine/files" {:success "DONE"}))))))))
 
 (defn edit
   [{:keys [params] :as req}]
@@ -106,60 +94,37 @@
   [{:keys [params web] :as req}]
   (fp/with-fallback #(upload (assoc req :problems %))
     (let [{:keys [sengine-addr]} web
-          values (check-empty-file (fp/parse-params form/upload-form params))
-          error-prefix "Error while uploading file to sengine: "]
+          values (check-empty-file (fp/parse-params form/upload-form params))]
       (cond
         (not (contains? values :file))
         (error! [:file] "You should specify file!")
-
         :else
         (let [{:keys [id file]} values
               {:strs [tempfile filename]} file
               url (format "http://%s/files/%s" sengine-addr id)
-              {:keys [status body error]} @(http/post url {:multipart [{:name     "file"
-                                                                        :content  tempfile
-                                                                        :filename filename}]})
-              response (json/parse-string body true)]
-          (cond
-            error (render "sengine/upload" (->> error
-                                                (wrap-error error-prefix)
-                                                (vector)
-                                                (into {})
-                                                (assoc req :flash)) {})
-            (not= status 200) (render "sengine/upload" (->> response
-                                                            (error-response->string-message)
-                                                            (wrap-error error-prefix)
-                                                            (vector)
-                                                            (into {})
-                                                            (assoc req :flash)) {})
-            :else (do
-                    (audit/info req :replace-sengine-file (dissoc values :file))
-                    (redirect-with-flash "/sengine/files" {:success "DONE"}))))))))
+              error-prefix "Error while uploading file to sengine: "
+              resp @(http/post url {:multipart [{:name     "file"
+                                                 :content  tempfile
+                                                 :filename filename}]})
+              [_ error] (check-response resp error-prefix)]
+          (if error
+            (render-with-error "sengine/upload" req error {})
+            (do
+              (audit/info req :replace-sengine-file (dissoc values :file))
+              (redirect-with-flash "/sengine/files" {:success "DONE"}))))))))
 
 (defn delete
   [{:keys [web params] :as req}]
   (let [{:keys [sengine-addr]} web
         {:keys [id]} params
         url (format "http://%s/files/%s" sengine-addr id)
-        {:keys [status body error]} @(http/delete url)
-        response (json/parse-string body true)
-        error-prefix "Error while deleting file from sengine: "]
-    (cond
-      error (->> error
-                 (wrap-error error-prefix)
-                 (vector)
-                 (into {})
-                 (redirect-with-flash "/sengine/files"))
-      (not= status 200) (->> response
-                             (error-response->string-message)
-                             (wrap-error error-prefix)
-                             (vector)
-                             (into {})
-                             (redirect-with-flash "/sengine/files"))
-      :else (do
-              (audit/info req :delete-sengine-file {:id id})
-              (redirect-with-flash "/sengine/files"
-                                   {:success (format "File with id %s was deleted" id)})))))
+        error-prefix "Error while deleting file from sengine: "
+        [_ error] (check-response @(http/delete url) error-prefix)]
+    (if error
+      (redirect-with-flash "/sengine/files" {:error error})
+      (do
+        (audit/info req :delete-sengine-file {:id id})
+        (redirect-with-flash "/sengine/files" {:success (format "File with id %s was deleted" id)})))))
 
 (defn download
   [{:keys [params web] :as req}]
@@ -193,24 +158,46 @@
         {:keys [sengine-addr]} web
         event-id (str (UUID/randomUUID))
         url (format "http://%s/files/%s/%s" sengine-addr id event-id)
-        {:keys [status body error]} @(http/post url)
-        error-prefix "Error while creating session: "]
-    (cond
-      error (->> error
-                 (wrap-error error-prefix)
-                 (vector)
-                 (into {})
-                 (redirect-with-flash "/sengine/files"))
-      (not= status 200) (as-> body $
-                              (json/parse-string $ true)
-                              (error-response->string-message $)
-                              (wrap-error error-prefix $)
-                              (vector $)
-                              (into {} $)
-                              (redirect-with-flash "/sengine/files" $))
-      :else (do
-              (redirect-with-flash (format "/sengine/files/%s/profile/%s" id event-id) {})))))
+        error-prefix "Error while creating session: "
+        [_ error] (check-response @(http/post url) error-prefix)]
+    (if error
+      (redirect-with-flash "/sengine/files" {:error error})
+      (redirect-with-flash (format "/sengine/files/%s/profile/%s" id event-id) {}))))
 
-(defn view-profile [req]
-  (render "sengine/profile" req {}))
+(defn render-profile [req event-log out]
+  (render "sengine/profile" req {:event-log event-log
+                                 :out out}))
+
+(defn view-profile
+  [{:keys [params web] :as r}]
+  (let [{:keys [id event-id]} params
+        {:keys [sengine-addr]} web
+        url (format "http://%s/files/%s/%s/event-log"
+                    sengine-addr id event-id)
+        error-prefix "Error while getting event log: "
+        [event-log error] (check-response @(http/get url) error-prefix)]
+    (if error
+      (redirect-with-flash (:uri r) {:error error})
+      (let [url (format "http://%s/files/%s/%s/settlements"
+                        sengine-addr id event-id)
+            resp @(http/get url)
+            error-prefix "Error while getting settlements: "
+            [out error] (check-response resp error-prefix)]
+        (if error
+          (redirect-with-flash (:uri r) {:error error})
+          (render-profile r event-log out))))))
+
+(defn send-profile
+  [{:keys [params web] :as r}]
+  (let [{:keys [send-type event-log id event-id]} params
+        {:keys [sengine-addr]} web
+        url (format "http://%s/files/%s/%s/event-log/%s"
+                    sengine-addr id event-id send-type)
+        resp @(http/post url {:body event-log})
+        error-prefix "Error while setting event log: "
+        [_ error] (check-response resp error-prefix)]
+    (if error
+      (redirect-with-flash (:uri r) {:error error})
+      (let [url (format "/sengine/files/%s/profile/%s" id event-id)]
+        (redirect-with-flash url {:success "DONE"})))))
 
--- src/malt_admin/web.clj
+++ src/malt_admin/web.clj
@@ -62,6 +62,7 @@
   (DELETE "/sengine/files/:id" req (allow req :admin (sengine/delete req)))
   (GET "/sengine/files/:id/profile" req (allow req :any (sengine/init-profile-session req)))
   (GET "/sengine/files/:id/profile/:event-id" req (allow req :any (sengine/view-profile req)))
+  (POST "/sengine/files/:id/profile/:event-id" req (allow req :any (sengine/send-profile req)))
 
   (GET "/filler" req (allow req :admin (filler/index req)))
 
